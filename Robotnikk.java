5692f4bd2a0ed2792765e0262fc1a51a43285cf3
ItaloSLeao <italoleao200656@gmail.com>
2026-06-19 18:03:42 -0300

    initial commit

---
+++ Robotnikk.java
@@ -0,0 +1,18 @@
+package si;
+
+import robocode.*;
+import robocode.util.Utils;
+import java.awt.Color;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Robotnikka, o Robô deus do Sol vê o futuro dos inimigos e antecipa.
+ *
+ * Faz a previsao com um algoritmo de predicao baseado em KNN e arvores
+ * K-dimensionais para atirar onde o alvo estara futuramente.
+ */
+public class Robotnikk extends AdvancedRobot {}
